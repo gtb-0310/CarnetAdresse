d8061c32d726467aae529d0debe3e97dbe206ba6
Geoffrey <geoffreyt1996@gmail.com>
2022-06-11 21:00:40 +0200

    Ajout des tests d'intégration

---
+++ TestCarnetAdresse/IntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using CarnetAdresseXamarin.Services;
+
+namespace TestCarnetAdresse
+{
+    public class IntegrationTest
+    {
+        [Fact]
+        public async Task testGetItemAsync()
+        {
+            var mkdtStore = new MockDataStore();
+            var testFunc = mkdtStore.GetItemAsync("62a24919ee043e14e4e4309a");
+            Assert.NotNull(testFunc);
+
+        }
+
+
+        [Fact]
+        public async Task tesDeleteItemAsync()
+        {
+            var mkdtStore = new MockDataStore();
+            var testFunc = mkdtStore.DeleteItemAsync("62a24919ee043e14e4e4309a");
+            Assert.NotNull(testFunc);
+        }
+    }
+}
